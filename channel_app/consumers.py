8c0e8493c3b09e4729270a0b360b57c1352015d1
Khushi <Khushipandey8292@gmail.com>
2025-04-09 19:02:41 +0530

    two-way communication

--- channel_app/consumers.py
+++ channel_app/consumers.py
@@ -2,6 +2,7 @@ from channels.consumer import SyncConsumer,AsyncConsumer
 from channels.exceptions import StopConsumer
 from time import sleep
 import asyncio
+import json
 class MySyncConsumer(SyncConsumer):
     def websocket_connect(self,event):
         print("websocket connected...",event)
@@ -10,11 +11,10 @@ class MySyncConsumer(SyncConsumer):
         })
     def websocket_receive(self,event):
         print("Message received from client",event)   
-        print(event["text"])
-        for i in range(10):
+        for i in range(20):
             self.send({
                 "type":"websocket.send",
-                "text":str(i)
+                "text":json.dumps({"count":i})
             })
             sleep(1)   
     def websocket_disconnect(self,event):
@@ -22,21 +22,21 @@ class MySyncConsumer(SyncConsumer):
         raise StopConsumer()
     
     
-class MyAsyncConsumer(AsyncConsumer):
-    async  def websocket_connect(self,event):
-        print("websocket connected...")
-        await self.send({
-            "type": "websocket.accept"
-        })
+# class MyAsyncConsumer(AsyncConsumer):
+#     async  def websocket_connect(self,event):
+#         print("websocket connected...")
+#         await self.send({
+#             "type": "websocket.accept"
+#         })
         
-    async def websocket_receive(self,event):
-        print("Message received from client",event)   
-        print(event['text'])
-        await self.send({
-            "type":"websocket.send",
-            "text":"message sent to client"
-        }) 
-        
-    async def websocket_disconnect(self,event):
-        print("websocket Disconnected...",event)
-        raise StopConsumer()
+#     async def websocket_receive(self,event):
+#         print("Message received from client",event)   
+#         for i in range(20):
+#             await self.send({
+#                 "type":"websocket.send",
+#                 "text":f"message {i} sent to client"
+#             }) 
+#             await asyncio.sleep(1)
+#     async def websocket_disconnect(self,event):
+#         print("websocket Disconnected...",event)
+#         raise StopConsumer()
--- channel_app/routing.py
+++ channel_app/routing.py
@@ -3,5 +3,5 @@ from .import consumers
 
 websocket_urlpatterns=[
     path('ws/sc/',consumers.MySyncConsumer.as_asgi()),
-    path('ws/ac/',consumers.MyAsyncConsumer.as_asgi()),
+    #  path('ws/ac/',consumers.MyAsyncConsumer.as_asgi()),
 ]
